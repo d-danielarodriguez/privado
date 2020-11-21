6e37316dbd8817a280bf43b49c6a3ebc0fa582d4
daniela <daniela@github.com>
2020-11-21 15:50:28 -0600

    version1

---
+++ src/app/Components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+//interface
+//const
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+  })
+  export class HomeComponent implements OnInit {
+
+    //info = INFORMACION;
+  
+    constructor(private router: Router) { }
+  
+    ngOnInit(): void {
+    }
+  
+    navegar(link: string){
+      const liga = '/' + link;
+      this.router.navigate([liga]);
+    }
+  
+  }
