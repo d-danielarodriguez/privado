4af909e84e6a4278e29f019d99687251af784b73
daniela <daniela@github.com>
2020-11-26 08:12:47 -0600

    guardando datos

--- src/app/Components/recibo/recibo.component.ts
+++ src/app/Components/recibo/recibo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+//import { ConexionBdService } from './../../Services/conexion-bd.service';
 
 @Component({
   selector: 'app-recibo',
@@ -10,10 +11,18 @@ export class ReciboComponent implements OnInit {
   query: { fecha: any; cantEnerg: any; cantPagar: any; };
 
   constructor() { }
-
+ //constructor(private bdService: ConexionBdService) {}
   ngOnInit(): void {
   }
-
+  //this.http.post<any>('https://jsonplaceholder.typicode.com/posts', { title: 'Angular POST Request Example' }).subscribe(data => {
+    //this.postId = data.id;
+  //})
+  //ngOnInit(): void {
+    //this.bdService.llaveActual.subscribe(llave => this.id = llave);
+    //this.bdService.getDomicilio(this.id).subscribe((data: any) => {
+      //this.info = data[0];
+    //});
+  //}
   onSubmit(form: NgForm){
     const {fecha, cantEnerg, cantPagar} = form.value;
     this.query={fecha, cantEnerg, cantPagar};
